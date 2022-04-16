add63fa7fe2fa05cbcf51275bf15703a70dbbeea
Jitender <58380090+jeetu9896@users.noreply.github.com>
2022-04-16 22:25:29 +0530

    first

---
+++ minimum_platfrom.js
@@ -0,0 +1,24 @@
+function runProgram(input) {
+}
+if (process.env.USERNAME ==='Jitender') {
+  runProgram(`6
+  9:00 9:40 9:50 11:00 15:00 18:00
+  9:10 12:00 11:20 11:30 19:00 20:00`);
+} else {
+  process.stdin.resume();
+  process.stdin.setEncoding("ascii");
+  let read = "";
+  process.stdin.on("data", function (input) {
+    read += input;
+  });
+  process.stdin.on("end", function () {
+    read = read.replace(/\n$/, "");
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+  });
+  process.on("SIGINT", function () {
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+    process.exit(0);
+  });
+}
